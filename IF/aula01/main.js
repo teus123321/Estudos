191c7e0b90e1aad43cfdbb1296db7adf1e6aaf14
Mateus <mateushgds75@gmail.com>
2026-08-04 16:02:31 -0400

    implementando melhorias para usuário efetuar login

--- IF/aula01/main.js
+++ IF/aula01/main.js
@@ -3,17 +3,18 @@ const email = document.getElementById("email")
 const senha = document.getElementById("password")
 
 
-button.addEventListener("click", function (event) {
+button.addEventListener("click", (event) => {
 
     event.preventDefault();
 
-    if (email.value.trim() === "" || senha.value.trim() === "") {
-        alert("Por favor, preencha todos os campos.");
-
-    } else {
+    if (!email.value.endsWith("@gmail.com") || email.value.length <= 10) {
+        alert("Email inválido ou senha inválida!");
+    }
+    else if (senha.value.length < 6) {
+        alert("Senha inválida!");
+    }
+    else {
         alert("Login realizado com sucesso!");
         window.location.href = "index.html";
     }
-
 })
-
